793dd5196be2ad5d8d1a3973b8beb4740378d7ad
Celestin <celestin.basura@gmail.com>
2014-04-10 01:19:19 +0200

    testing commin

---
+++ app/src/main/java/com/celeMC/tonymodbus/app/GroupList.java
@@ -0,0 +1,7 @@
+package com.celeMC.tonymodbus.app;
+
+/**
+ * Created by Celestin on 10.4.2014..
+ */
+public class GroupList {
+}
---
+++ app/src/main/java/com/celeMC/tonymodbus/app/Models/InteriorPoint.java
@@ -0,0 +1,7 @@
+package com.celeMC.tonymodbus.app.Models;
+
+/**
+ * Created by Celestin on 10.4.2014..
+ */
+public class InteriorPoint {
+}
